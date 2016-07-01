254a236b5682fb7b4beba5fc534b924734b22aa5
Giuseppe Pagano <giuseppe.pagano@seco.com>
2016-07-01 16:54:51 +0200

    Define bluetooth name for the board.

--- udoo_6dq/bluetooth/bdroid_buildcfg.h
+++ udoo_6dq/bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "iMX6"
+#define BTM_DEF_LOCAL_NAME "uDOO-iMX6"
 
 // Networking, Capturing, Object Transfer
 // MAJOR CLASS: COMPUTER
