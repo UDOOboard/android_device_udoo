a544436ab790bd27c2843aadd8b470e1badd0728
Giuseppe Pagano <giuseppe.pagano@seco.com>
2016-07-05 00:19:36 +0200

    Fix path in Boardconfig file

--- udoo_6dq/BoardConfig.mk
+++ udoo_6dq/BoardConfig.mk
@@ -10,9 +10,9 @@ ADDITIONAL_BUILD_PROPERTIES += \
 	ro.boot.storage_type=sd
 TARGET_RECOVERY_FSTAB = device/udoo/udoo_6dq/fstab.freescale
 PRODUCT_COPY_FILES +=	\
-	device/udoo/a62_6dq/fstab_sd.freescale:root/fstab.freescale     \
-	device/udoo/a62_6dq/fstab_sd.freescale:root/fstab.freescale.sd  \
-	device/udoo/a62_6dq/fstab_sata.freescale:root/fstab.freescale.sata
+	device/udoo/udoo_6dq/fstab_sd.freescale:root/fstab.freescale     \
+	device/udoo/udoo_6dq/fstab_sd.freescale:root/fstab.freescale.sd  \
+	device/udoo/udoo_6dq/fstab_sata.freescale:root/fstab.freescale.sata
 
 ADDITIONAL_BUILD_PROPERTIES += \
 	persist.udoo_enable_adk=false
