b1cd8dd5dd7aa38273ad00c76a41ef28919d6e0c
Giuseppe Pagano <giuseppe.pagano@seco.com>
2016-07-22 21:32:57 +0200

    Fix lvds7+hdmi support, add support for LVDS 15''

--- a62_6dq/BoardConfig.mk
+++ a62_6dq/BoardConfig.mk
@@ -73,7 +73,7 @@ ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 BOARD_KERNEL_CMDLINE +=  mtdparts=gpmi-nand:16m(bootloader),16m(bootimg),128m(recovery),-(root) gpmi_debug_init ubi.mtd=3
 endif
 
-BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 init=/init video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:dev=hdmi,1920x1080M@60,bpp=32 video=mxcfb2:off video=mxcfb3:off vmalloc=256M androidboot.console=ttymxc1 consoleblank=0 androidboot.hardware=freescale cma=384M androidboot.selinux=disabled androidboot.dm_verity=disabled no_console_suspend  
+BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 init=/init vmalloc=256M androidboot.console=ttymxc1 consoleblank=0 androidboot.hardware=freescale cma=384M androidboot.selinux=disabled androidboot.dm_verity=disabled no_console_suspend  
 
 BOARD_HAVE_BLUETOOTH        	:= true
 BOARD_HAVE_BLUETOOTH_USB        := true
@@ -98,12 +98,12 @@ TARGET_BOARD_DTS_CONFIG := imx6q:imx6q-seco_A62.dtb imx6dl:imx6dl-seco_A62.dtb
 TARGET_KERNEL_VERSION := kernel_imx
 TARGET_KERNEL_DEFCONF := imx_v7_android_A62_defconfig
 TARGET_KERNEL_MODULES += \
-	arch/arm/boot/dts/imx6dl-seco_A62.dtb:system/dts/imx6dl-a62-hdmi.dtb \
-	arch/arm/boot/dts/imx6dl-seco_A62.dtb:system/dts/imx6dl-a62-lvds7.dtb \
-	arch/arm/boot/dts/imx6dl-seco_A62.dtb:system/dts/imx6dl-a62-lvds15.dtb \
-	arch/arm/boot/dts/imx6q-seco_A62.dtb:system/dts/imx6q-a62-hdmi.dtb \
-	arch/arm/boot/dts/imx6q-seco_A62.dtb:system/dts/imx6q-a62-lvds7.dtb \
-	arch/arm/boot/dts/imx6q-seco_A62.dtb:system/dts/imx6q-a62-lvds15.dtb \
+	arch/arm/boot/dts/imx6dl-seco_A62-hdmi.dtb:system/dts/imx6dl-a62-hdmi.dtb \
+	arch/arm/boot/dts/imx6dl-seco_A62-lvds7.dtb:system/dts/imx6dl-a62-lvds7.dtb \
+	arch/arm/boot/dts/imx6dl-seco_A62-lvds15.dtb:system/dts/imx6dl-a62-lvds15.dtb \
+	arch/arm/boot/dts/imx6q-seco_A62-hdmi.dtb:system/dts/imx6q-a62-hdmi.dtb \
+	arch/arm/boot/dts/imx6q-seco_A62-lvds7.dtb:system/dts/imx6q-a62-lvds7.dtb \
+	arch/arm/boot/dts/imx6q-seco_A62-lvds15.dtb:system/dts/imx6q-a62-lvds15.dtb \
 
 PRODUCT_COPY_FILES +=	\
 	device/udoo/a62_6dq/uEnv.txt:system/uEnv.txt
