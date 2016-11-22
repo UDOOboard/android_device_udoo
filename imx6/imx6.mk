fed05b10ca17af550c99a9057aa82d432ce6e406
Francesco Montefoschi <francesco.monte@gmail.com>
2016-11-22 15:40:42 +0100

    Added libemoji

--- imx6/imx6.mk
+++ imx6/imx6.mk
@@ -29,6 +29,7 @@ PRODUCT_PACKAGES += \
 	VisualizationWallpapers               \
 	CubeLiveWallpapers                    \
 	PinyinIME                             \
+	libemoji                              \
 	libjni_pinyinime                      \
 	libRS                                 \
 	chat                                  \
