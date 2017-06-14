5162a48d8d0ee4042635157f6aed76d11f677aa8
Francesco Montefoschi <francesco.monte@gmail.com>
2017-06-14 10:37:25 +0200

    secuflag: Edit BOARD_KERNEL_CMDLINE * Fix: imx6.gralloc: <initialize,352> open /dev/fb0 failed

--- udoo_6dq/BoardConfig.mk
+++ udoo_6dq/BoardConfig.mk
@@ -48,7 +48,7 @@ TARGET_SELECT_KEY := 28
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 DM_VERITY_RUNTIME_CONFIG := true
 
-BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 init=/init video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:off video=mxcfb2:off video=mxcfb3:off vmalloc=128M androidboot.console=ttymxc1 consoleblank=0 androidboot.hardware=freescale cma=448M androidboot.dm_verity=disabled
+BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 init=/init vmalloc=128M androidboot.console=ttymxc1 consoleblank=0 androidboot.hardware=freescale cma=448M androidboot.dm_verity=disabled
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 #UBI boot command line.
